1604b6d2d765144e0194f4ad41d2926ae123d1d1
shafikshaon <shafikshaon@gmail.com>
2017-12-22 13:55:31 +0600

    Account model added

---
+++ Eatm/Account.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eatm
+{
+    class Account
+    {
+        public string FullName { get; set; }
+        public int CardNumber { get; set; }
+        public int PinCode { get; set; }
+        public double Balance { get; set; }
+    }
+}
