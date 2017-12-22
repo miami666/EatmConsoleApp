93c5e8f9223823f78d7fedde9935a2d077a15b5e
shafikshaon <shafikshaon@gmail.com>
2017-12-22 15:40:24 +0600

    Card Number verification

--- Eatm/Eatm.cs
+++ Eatm/Eatm.cs
@@ -8,8 +8,16 @@ namespace Eatm
 {
     class Eatm
     {
+        private string _loginPrompt;
+        private string _cardInput;
+        private string _cardInputError;
         private string _loginChoicePrompt;
         private string _loginChoiceError;
+        private string _pinInput;
+        private string _pinInputError;
+        private string _cardNotFound;
+
+        private List<Account> _accountList;
 
         public void Init()
         {
@@ -19,6 +27,18 @@ namespace Eatm
 
 Enter your choice: ";
             _loginChoiceError = "Wrong choice! Please try again";
+            _cardInput = "Enter your card number";
+            _cardInputError = "Wrong card number! Try again";
+            _pinInput = "Enter your pin code";
+            _pinInputError = "Wrong pin code! Try again";
+            _cardNotFound = "Card number not found! Please try again";
+
+            _accountList = new List<Account>
+            {
+                new Account() { FullName = "Shafik Shaon", CardNumber = 123, PinCode = 1111, Balance = 20000 },
+                new Account() { FullName = "Sadia Sabnaj", CardNumber = 456, PinCode = 2222, Balance = 15000 },
+                new Account() { FullName = "Shafikur Rahman", CardNumber = 789, PinCode = 3333, Balance = 29000 }
+            };
         }
 
         public void Start()
@@ -35,7 +55,20 @@ Enter your choice: ";
 
         private void NormalUser()
         {
-            Console.WriteLine("Normal User");
+            Account account = GetNormalUser();
+            if (account == null)
+            {
+                Console.WriteLine(_cardNotFound);
+                NormalUser();
+            }
+        }
+
+        private Account GetNormalUser()
+        {
+            var cardNumber = TakeUserInput(_cardInput, _cardInputError);
+            foreach (Account normalUser in _accountList)
+                if (cardNumber == normalUser.CardNumber) return normalUser;
+            return null;
         }
 
         private void Admin()
